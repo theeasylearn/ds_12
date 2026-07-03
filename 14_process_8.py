89242b4dea9615da3097bd2bbc4fa8b3a33c8c93
theeasylearn <theeasylearn@gmail.com>
2026-07-03 12:20:11 +0530

    process

--- 14_process_8.py
+++ 14_process_8.py
@@ -1,21 +1,18 @@
-# write a program to findout BMI (Body to mass index) of given weight and height 
-#accept height
-print("Enter your height (in feet and inch)")
-foot = int(input("Enter only feet "))
-inch = int(input("Enter only remaining inch"))
-#accept weight
-weight = float(input("enter your weight in kg"))
+# write a program to findout B.M.I. (Body to mass index) of given weight and height 
+#accept input
+weight = float(input("Enter weight in KG"))
+print("Enter your height in foot and inches")
+feet = int(input("Enter only feet"))
+inch = int(input("Enter only remaining inches"))
 
-#process 
-#calculate total inch
-total_inch = (foot * 12) + inch
+# process 
+# calculate total inch 
+total_inch = (feet * 12) + inch 
 
-#convert centimeter 
-meter = total_inch / 39.37
+#convert total inch into meter 
+meter = total_inch / 39.37 
 
 #calculate BMI 
 bmi = weight / (meter * meter)
-
-print(f"your BMI IS {bmi:.2f}")
-
+print(f"your bmi is {bmi:.2f}")
 
--- 15_process_9.py
+++ 15_process_9.py
@@ -1,6 +1,8 @@
 '''
 write a program to generate de-nomination of given amount in indian currency 
-input: 886 
+assume person amit wants to give 886 rupees to meena without accept any changes. amit will given this amount using biggest possible currency note. 
+
+input: 888 
  500 x 1 = 500
  200 x 1 = 200
  100 x 1 = 100
@@ -8,42 +10,37 @@ input: 886
  20 x 1 =   20
  10 x 1 =   10
  5 x 1 =    05
- 1 x 1 =    0
+ 2 x 1 =    02
+ 1 x 1 =    01
 '''
-amount = int(input("Enter amount")) 
-five_hd = amount // 500
-amount = amount - (five_hd * 500) 
 
-two_hd = amount // 200
-amount = amount - (two_hd * 200) 
-
-one_hd = amount // 100
-amount = amount - (one_hd * 100) 
-
+#assume amount is 888
+amount = int(input("Enter amount"))
+five_hundred = amount // 500
+amount = amount - (five_hundred * 500)
+two_hundred = amount // 200
+amount = amount - (two_hundred * 200)
+one_hundred = amount // 100
+amount = amount - (one_hundred * 100)
 fifty = amount // 50
-amount = amount - (fifty * 50) 
-
+amount = amount - (fifty * 50)
 twenty = amount // 20
-amount = amount - (twenty * 20) 
-
+amount = amount - (twenty * 20)
 ten = amount // 10
-amount = amount - (ten * 10) 
-
+amount = amount - (ten * 10)
 five = amount // 5
-amount = amount - (five * 5) 
-
+amount = amount - (five * 5)
 two = amount // 2
-amount = amount - (two * 2) 
-
+amount = amount - (two * 2)
 one = amount // 1
-amount = amount - (one * 1) 
+amount = amount - (one * 1)
 
-print(f"Five hundred = {five_hd}")
-print(f"two hundred = {two_hd}")
-print(f"one hundred = {one_hd}")
-print(f"Fifty = {fifty}")
-print(f"twenty = {twenty}")
-print(f"ten = {ten}")
-print(f"five = {five}")
-print(f"two = {two}")
-print(f"one = {one}")
+print("500 X ",five_hundred)
+print("200 X ",two_hundred)
+print("100 X ",one_hundred)
+print("50 X ",fifty)
+print("20 X ",twenty)
+print("10 X ",ten)
+print("5 X ",five)
+print("2 X ",two)
+print("1 X ",one)
