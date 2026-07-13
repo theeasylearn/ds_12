6f0a26e59e092cb22a5892684c5d7f2dcf362430
theeasylearn <theeasylearn@gmail.com>
2026-07-13 15:29:47 +0530

    unit 1 and 2 finished

---
+++ 28_assignment_operator.py
@@ -0,0 +1,27 @@
+
+a = 10
+b = 3
+
+# a = a + b or 
+a += b
+print(f"after addition a = {a} = b = {b}")
+
+# a = a - b
+a-=b 
+print(f"after subtraction a = {a} = b = {b}")
+
+# a = a * b 
+a *= b
+print(f"after multiplication a = {a} = b = {b}")
+
+# a = a / b
+a /= b 
+print(f"after division a = {a} = b = {b}")
+
+# a = a ** b 
+a **= b
+print(f"after power a = {a} = b = {b}")
+
+# a = a // b
+a//=b 
+print(f"after floor a = {a} = b = {b}")
