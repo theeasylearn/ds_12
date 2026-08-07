25afb2de55429e23f4f1614729dfc07a82a7b492
theeasylearn <theeasylearn@gmail.com>
2026-08-07 16:20:51 +0530

    oop

---
+++ 66_oop1.py
@@ -0,0 +1,12 @@
+class Animal:
+    def eat(self):
+        print("Animal can eat")
+    def run(self):
+        print("Animal can run")
+    def sleep(self):
+        print("Animal can sleep")
+#create object
+Tiger = Animal()
+Tiger.eat()
+Tiger.run()
+Tiger.sleep()
